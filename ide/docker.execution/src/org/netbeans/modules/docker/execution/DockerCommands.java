db6a366d6b0fb312e1f512702fe9556ea1681217
Bogdan Haidu <haidubogdan@gmail.com>
2025-08-29 16:29:38 +0300

    work in progress npm and node docker config executable

---
+++ ide/docker.execution/src/org/netbeans/modules/docker/execution/DockerCommands.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.docker.execution;
+
+/**
+ *
+ * @author bhaidu
+ */
+public final class DockerCommands {
+
+    public static final String DOCKER_BASE_COMMAND = "docker"; // NOI18N
+    public static final String DOCKER_EXEC = "exec"; // NOI18N
+    
+    public static final String DOCKER_COMMAND_OPTION = "c"; // NOI18N
+    public static final String DOCKER_INTERACTIVE_OPTION = "i"; // NOI18N
+    public static final String DOCKER_TTY_OPTION = "t"; // NOI18N
+    public static final String DOCKER_WORKDIR_OPTION = "w"; // NOI18N
+    public static final String DOCKER_USER_OPTION = "u"; // NOI18N
+}
