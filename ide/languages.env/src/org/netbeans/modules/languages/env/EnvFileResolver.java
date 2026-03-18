d576f44553a77a8bbffea3773d9f79e704d2e55e
Bogdan Haidu <haidubogdan@gmail.com>
2026-03-18 22:11:34 +0200

    env file lexer and mime association fixes
    - additional coloring lexer fix for single quoted string with slash
    - adjust env file resolver to match files starting in ".env."
    - exclude files with lexer and errors extension from env mime type

--- ide/languages.env/src/org/netbeans/modules/languages/env/EnvFileResolver.java
+++ ide/languages.env/src/org/netbeans/modules/languages/env/EnvFileResolver.java
@@ -18,6 +18,7 @@
  */
 package org.netbeans.modules.languages.env;
 
+import java.util.Set;
 import org.netbeans.api.annotations.common.CheckForNull;
 import org.netbeans.api.annotations.common.NonNull;
 import org.openide.awt.ActionID;
@@ -92,8 +93,16 @@ import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = MIMEResolver.class)
 public class EnvFileResolver extends MIMEResolver {
+
     public static final String ENV_EXT = "env"; //NOI18N
+    public static final String DOT_ENV = "." + ENV_EXT; //NOI18N
     public static final String MIME_TYPE = "text/x-env"; //NOI18N
+
+    /**
+     * extensions to exclude from mime association
+     * ex: compiled golden test files like *.lexer, *.error
+     */
+    private static final Set<String> EXCLUDED_EXTENSIONS = Set.of("lexer", "errors"); //NOI18N
 
     public EnvFileResolver() {
         super(MIME_TYPE);
@@ -101,22 +110,26 @@ public class EnvFileResolver extends MIMEResolver {
 
     @CheckForNull
     @Override
-    public String findMIMEType(@NonNull final FileObject fo) {
-        final String nameWithExt = fo.getNameExt().toLowerCase();
-
-        if (nameWithExt.endsWith("." + ENV_EXT)) { //NOI18N
-            return MIME_TYPE;
-        }
-
-        //Some application might use .env.example name format
-        int envPartPosition = 2;
-        String[] nameParts = nameWithExt.split("\\."); //NOI18N
-
-        //check for previous name part
-        if (nameParts.length >= envPartPosition && nameParts[nameParts.length - envPartPosition].equals(ENV_EXT)) {
+    public String findMIMEType(@NonNull final FileObject file) {
+        if (isEnvFile(file)) {
             return MIME_TYPE;
         }
 
         return null;
+    }
+
+    public static boolean isEnvFile(FileObject file) {
+        final String ext = file.getExt();
+
+        if (EXCLUDED_EXTENSIONS.contains(ext)) {
+            return false;
+        }
+
+        final String nameWithExt = file.getNameExt().toLowerCase();
+        return ext.equals(ENV_EXT)
+                || nameWithExt.equals(DOT_ENV)
+                //env files naming usually used in JS, PHP, Python frameworks
+                //.env.dist, .env.local, .env.local.demo ...
+                || nameWithExt.startsWith(DOT_ENV + ".");
     }
 }
